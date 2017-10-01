b5ed59ec8e2228e445b321b95b9abb4f385e6fe6
Gerson Sosa <gersonsosa@gmail.com>
2017-10-01 06:28:51 -0500

    Adds hackerrank brackets solution

--- src/main/java/com/gersonsosa/projecteuler/Brackets.java
+++ src/main/java/com/gersonsosa/projecteuler/Brackets.java
@@ -4,14 +4,14 @@ import java.util.Stack;
 
 public class Brackets {
 
-    public boolean isBalanced(String expression) {
-       Stack<Character> openBrackets = new Stack<>();
+    public static boolean isBalanced(String expression) {
+        Stack<Character> openBrackets = new Stack<>();
         for (int i = 0; i < expression.length(); i++) {
             char current = expression.charAt(i);
-            if (current == '{' || current == '[' || current == '(' ) {
+            if (current == '{' || current == '[' || current == '(') {
                 openBrackets.push(current);
-            }else if (current == '}' || current == ']' || current == ')' ) {
-                if (openBrackets.isEmpty() || !matches(openBrackets.pop(), current)){
+            } else if (current == '}' || current == ']' || current == ')') {
+                if (openBrackets.isEmpty() || !matches(openBrackets.pop(), current)) {
                     return false;
                 }
             }
@@ -19,7 +19,7 @@ public class Brackets {
         return openBrackets.empty();
     }
 
-    private boolean matches(char pop, char current) {
+    private static boolean matches(char pop, char current) {
         return (pop == '{' && current == '}') || (pop == '[' && current == ']') || pop == '(' && current == ')';
     }
 }
